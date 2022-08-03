67e49f15c59680a6d865c6dfd2781b19c8f84e70
pepe6bf <pepe6bf@gmail.com>
2022-08-04 06:38:46 +0900

    #3 Spring Boot Project Setting

---
+++ exhibition-platform/src/test/java/com/hostit/exhibitionplatform/ExhibitionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostit.exhibitionplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
