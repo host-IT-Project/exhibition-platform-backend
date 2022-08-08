e03b874c02bebebb21b8495bf70ecaac7662b2a9
pepe6bf <pepe6bf@gmail.com>
2022-08-09 00:10:38 +0900

    feat(global): #5 Audit 기능 추가
    엔티티 전체의 적용될 audit 기능 추가

---
+++ exhibition-platform/src/main/java/com/hostit/exhibitionplatform/global/config/audit/BaseTimeEntity.java
@@ -0,0 +1,31 @@
+package com.hostit.exhibitionplatform.global.config.audit;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
+
