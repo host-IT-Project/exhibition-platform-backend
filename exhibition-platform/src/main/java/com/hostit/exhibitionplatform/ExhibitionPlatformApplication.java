530b3c31e9212e3eac43df2a8be02d6acbc8fc71
pepe6bf <pepe6bf@gmail.com>
2022-08-09 00:18:50 +0900

    Main Application 추가

--- exhibition-platform/src/main/java/com/hostit/exhibitionplatform/ExhibitionPlatformApplication.java
+++ exhibition-platform/src/main/java/com/hostit/exhibitionplatform/ExhibitionPlatformApplication.java
@@ -1,9 +1,16 @@
 package com.hostit.exhibitionplatform;
 
+import com.hostit.exhibitionplatform.global.config.properties.AppProperties;
+import com.hostit.exhibitionplatform.global.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		CorsProperties.class,
+		AppProperties.class
+})
 public class ExhibitionPlatformApplication {
 
 	public static void main(String[] args) {
