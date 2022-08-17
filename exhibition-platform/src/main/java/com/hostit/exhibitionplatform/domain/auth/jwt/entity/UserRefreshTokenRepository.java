a3113c5eb4dcab7e2fd7042d16394c5f580a38de
pepe6bf <pepe6bf@gmail.com>
2022-08-17 17:27:00 +0900

    기존 프로젝트 삭제
    리펙토링한 프로젝트를 적용하기 위해 기존 프로젝트를 삭제한다.

--- exhibition-platform/src/main/java/com/hostit/exhibitionplatform/domain/auth/jwt/entity/UserRefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hostit.exhibitionplatform.domain.auth.jwt.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
-    UserRefreshToken findByUserId(String userId);
-
-    UserRefreshToken findByUserIdAndRefreshToken(String userId, String refreshToken);
-}
